4fcddabf1131caebc8c77aa934dcc8f69319b9e4
Mathsyo <mathiasdebarros@protonmail.com>
2022-11-12 09:35:26 +0100

    Updated

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -2,13 +2,19 @@
 
 @section('content')
 
-    <ul class="list-group-item">
-        @foreach ($messages as $message)
-            <li class="list-group-item">
-                {{ $message->content }}
-                <a href="/messages/{{ $message->id }}">Leer</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container py-5 my-5">
+        <ul class="list-group">
+            @foreach ($messages as $message)
+                <li class="list-group-item p-4 rounded-4">
+                    <span class="text-muted">De : </span>
+                    <a href="mailto:{{ $message->email }}">{{ $message->email }}</a>
+                    <br>
+                    <span class="text-muted">Contenu : </span>
+                    <br>
+                    {{ $message->content }}
+                </li>
+            @endforeach
+        </ul>
+</div>
 
 @endsection
